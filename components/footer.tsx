ae7e43111714666fc130323dae7f3b8a09ffe925
Heiko Plümer <heiko.pluemer@googlemail.com>
2025-04-25 16:14:15 +0200

    rc-2

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,47 +10,12 @@ export function Footer() {
   const { t, lang } = useTranslation();
   const currentYear = new Date().getFullYear();
 
-  const sections = [
-    // {
-    //   title: t('footer.product'),
-    //   links: [
-    //     { name: t('footer.links.overview'), href: '#' },
-    //     { name: t('footer.links.pricing'), href: '#' },
-    //     { name: t('footer.links.features'), href: '#' },
-    //   ],
-    // },
-    // {
-    //   title: t('footer.company'),
-    //   links: [
-    //     { name: t('footer.links.about'), href: '#' },
-    //     { name: t('footer.links.team'), href: '#' },
-    //     { name: t('footer.links.blog'), href: '#' },
-    //     { name: t('footer.links.contact'), href: '#' },
-    //   ],
-    // }
-  ];
-
   return (
       <section className="py-32">
         <div className="container">
           <footer>
             <Separator className="my-14" />
             <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-4">
-              {sections.map((section, sectionIdx) => (
-                  <div key={sectionIdx}>
-                    <h3 className="mb-4 font-bold">{section.title}</h3>
-                    <ul className="space-y-4 text-muted-foreground">
-                      {section.links.map((link, linkIdx) => (
-                          <li
-                              key={linkIdx}
-                              className="font-medium hover:text-primary"
-                          >
-                            <a href={link.href}>{link.name}</a>
-                          </li>
-                      ))}
-                    </ul>
-                  </div>
-              ))}
               <div>
                 <h3 className="mb-4 font-bold">{t('footer.tools')}</h3>
                 <ul className="space-y-4 text-muted-foreground">
